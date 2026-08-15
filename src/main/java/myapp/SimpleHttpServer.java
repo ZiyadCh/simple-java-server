99a29d5b1f9a472332cc9f2ec898b1f2da878231
ZiyadCh <chetoziyad@gmail.com>
2026-08-15 12:03:37 +0100

    basic server socket

--- src/main/java/myapp/SimpleHttpServer.java
+++ src/main/java/myapp/SimpleHttpServer.java
@@ -1,10 +1,18 @@
 package myapp;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /**
  * SimpleHttpServer
  */
 public class SimpleHttpServer {
   public static void main(String[] args) {
     final int port = 8000;
+    try (ServerSocket serversocket = new ServerSocket(port);) {
+      serversocket.accept();
+    } catch (IOException e) {
+      // TODO: handle exception
+    }
   }
 }
